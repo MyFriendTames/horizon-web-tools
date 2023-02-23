74f18b0ff1f0ac8c24dd8dcdc10470eb6a94c78f
tames <thamesdiego@gmail.com>
2023-02-23 11:24:40 -0600

    adding element-graph module

---
+++ src/elements/element-graph.js
@@ -0,0 +1,28 @@
+import { isElement, getProperty } from './elements.js';
+
+export const graphIDAttributeName = 'data-hwt-graph-index';
+
+function ElementGraph( root ){
+  if ( isElement( root ) ){
+    this.root = root;
+    this.graph = [];
+    const buildGraph = ( graph, elements, rootPath ) => {
+      for ( const element of elements ){
+        element.setAttribute( graphIDAttributeName, `${ rootPath?.concat( '.' ) || '' }${ graph.length }` );
+        const pseudoElement = {
+          graph: this,
+          element: element,
+          children: []
+        };
+        graph.push( pseudoElement );
+        pseudoElement.children = buildGraph( pseudoElement.children, element.children, `${ rootPath?.concat( '.' ) || '' }${ graph.length }` );
+      }
+    };
+    this.appendTo = function( parent, ...elements ){
+      const ID = parent.getAttribute( graphIDAttributeName );
+      const graph = getProperty( ID )?.children;
+      buildGraph( graph, elements, ID.replace( /\.?\d+$/ ) );
+    }
+    buildGraph( this.graph, root.children );
+  }else throw new Error( 'Not an HTMLElement' );
+}
